47f451ce15df28de56ef988fe5fda7ea434b4670
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 19:14:31 +0100

    Add files via upload

---
+++ pgcd_euclidienRécursif.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+def pgcd_recursif(a, b):
+  if (a > b):
+      a = a % b
+  else:
+      b = b % a
+  if (a == 0):
+      return b
+  if (b == 0):
+      return a
+  return pgcd_recursif(a, b)
+
+
+a = int(input(“Entrez le premier nombre : “))
+b = int(input(“Entrez le second nombre :”))
+print (“Leur pgcd est :”, pgcd_recursif(a, b))
+
+
+----------------------------------------------------------------------- Résultat ---------------------------------------------------------------------------
